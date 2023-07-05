8a75d28eb410fd55be850251d88658309429c50c
Vyacheslav Dobrynin <w.v.dobr@gmail.com>
2023-07-05 19:57:30 +0400

    feat: update lucene deps

--- lucene/build.gradle.kts
+++ lucene/build.gradle.kts
@@ -1,6 +1,5 @@
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
-    implementation("org.apache.lucene:lucene-core:9.5.0")
-    implementation("org.apache.lucene:lucene-queryparser:9.5.0")
-    implementation("org.apache.lucene:lucene-grouping:9.5.0")
+    implementation("org.apache.lucene:lucene-core:9.7.0")
+    implementation("org.apache.lucene:lucene-queryparser:9.7.0")
+    implementation("org.apache.lucene:lucene-grouping:9.7.0")
 }
