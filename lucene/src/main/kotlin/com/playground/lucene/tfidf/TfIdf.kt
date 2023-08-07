58f92517783cd2fc69210cf10d34c515da006838
Vyacheslav Dobrynin <w.v.dobr@gmail.com>
2023-08-07 17:06:45 +0400

    refactor: move THRESHOLD_VALUE to const

--- lucene/src/main/kotlin/com/playground/lucene/tfidf/TfIdf.kt
+++ lucene/src/main/kotlin/com/playground/lucene/tfidf/TfIdf.kt
@@ -18,6 +18,7 @@ import java.nio.file.Paths
 private const val FIELD_NAME = "content"
 private const val MAX_SCORE = 15.438689
 private var currentMaxScore = Float.MIN_VALUE
+private const val THRESHOLD_VALUE = 0.1
 
 fun main(args: Array<String>) {
     val documents = loadDocuments(args.first())
@@ -67,7 +68,6 @@ fun Directory.search() = Searcher(this).use { searcher ->
         // this.similarity = ClassicSimilarity()
         searcher.searchAll(MatchAllDocsQuery()).forEach { scoreDoc ->
             val content = storedFields().document(scoreDoc.doc)[FIELD_NAME]
-            val thresholdValue = 0.1
             val iterator = reader.termVectors()[scoreDoc.doc, FIELD_NAME].iterator()
             val belowThresholdTokens = mutableSetOf<String>()
             var bytesRef: BytesRef?
@@ -82,7 +82,7 @@ fun Directory.search() = Searcher(this).use { searcher ->
                     termStatistics(term, reader.docFreq(term), reader.totalTermFreq(term))
                 ).score(termFreq.toFloat(), 1) // ).score(reader.totalTermFreq(term).toFloat(), 1)
                 val normalizedScore = score / MAX_SCORE
-                if (normalizedScore < thresholdValue) {
+                if (normalizedScore < THRESHOLD_VALUE) {
                     belowThresholdTokens += term.text()
                     println("term: ${term.text()}, termFreq=$termFreq, docCount=$docCount, score=$normalizedScore")
                 }
