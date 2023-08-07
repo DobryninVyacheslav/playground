be70affe86757a2a00bc9639fe5b881b1205d5c8
Vyacheslav Dobrynin <w.v.dobr@gmail.com>
2023-08-07 18:50:37 +0400

    feat: add analyze function

--- lucene/src/main/kotlin/com/playground/lucene/Preprocessing.kt
+++ lucene/src/main/kotlin/com/playground/lucene/Preprocessing.kt
@@ -1,5 +1,8 @@
 package com.playground.lucene
 
+import org.apache.lucene.analysis.Analyzer
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute
+
 private val regex = Regex("[^А-Яа-яA-Za-z0-9 ]")
 fun cleanText(text: String): String = text.replace(regex, " ")
 
@@ -7,3 +10,16 @@ fun extractTotalBow(texts: Collection<String>): Map<String, Int> =
     texts.flatMapTo(mutableListOf()) { text -> cleanText(text).lowercase().split(" ") }
         .groupingBy { it }
         .eachCount()
+
+fun Analyzer.analyze(text: String): List<String> {
+    val tokens = mutableListOf<String>()
+    val tokenStream = this.tokenStream(null, text)
+    val attr = tokenStream.addAttribute(CharTermAttribute::class.java)
+    tokenStream.reset()
+    while (tokenStream.incrementToken()) {
+        tokens.add(attr.toString())
+    }
+    tokenStream.end()
+    tokenStream.close()
+    return tokens
+}
